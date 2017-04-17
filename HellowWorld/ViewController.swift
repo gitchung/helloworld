d760cc9de556aec71e907344759d1ef544b5eafb
Chung jy <guriun@Chung-jyui-MacBook-6.local>
2017-04-17 22:01:57 +0900

    The last version.

--- HellowWorld/ViewController.swift
+++ HellowWorld/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet var bgImageView: UIImageView!
 
+  @IBOutlet var logoImageView: UIImageView!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  bgImageView.isHidden = true
+    logoImageView.isHidden = true
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func welcomBtnWasPressed(_ sender: Any) {
+    bgImageView.isHidden = false
+    logoImageView.isHidden = false
+  }
 
 }
 
