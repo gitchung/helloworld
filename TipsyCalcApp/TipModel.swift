83ac12a5f3c19be069c2a4d00140315a541366f0
Chung jy <guriun@Chung-jyui-MacBook-6.local>
2017-04-19 18:42:52 +0900

    First Time(4-19)

---
+++ TipsyCalcApp/TipModel.swift
@@ -0,0 +1,13 @@
+//
+//  TipModel.swift
+//  TipsyCalcApp
+//
+//  Created by Chung jy on 2017. 4. 19..
+//  Copyright © 2017년 Chung jy. All rights reserved.
+//
+
+import Foundation
+
+class TipModel {
+  
+}
